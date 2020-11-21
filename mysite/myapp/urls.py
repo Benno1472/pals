94823767eb5c171e68886343d07cf4d55d8647ad
Rajan Maghera <rmaghera@ualberta.ca>
2020-11-20 21:56:59 -0700

    Began basic templating.

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all/', views.all_posts, name="all_posts"),
+    path('profile/', views.my_profile, name="my_profile"),
+    path('create/', views.create_post, name="create_post"),
+    path('registration/login/', views.login, name="login")
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,22 @@
+from typing import ContextManager
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world. You are at the index of myapp.")
+    context = {}
+    return render(request, "myapp/index.html", context)
+
+def create_post(request):
+    return HttpResponse("Hello world. You are at the create post page.")
+
+def my_profile(request):
+    return HttpResponse("Hello world. You are at the my profile page.")
+
+def all_posts(request):
+    return HttpResponse("Hello world. You are at the all posts page.")
+
+def login(request):
+    context = {}
+    return render(request, "myapp/registration/login.html", context)
