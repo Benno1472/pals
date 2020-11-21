1da48c0f83865fe23235184cfb73ad1728a9391c
kakaname <youngwoo.seo9604@gmail.com>
2020-11-21 10:47:42 -0700

    id removed, django built-in used

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,31 +1,39 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 # Create your models here.
 # find person first, then get posts though ID
+# from myapp.models import Person, Comment, Post
+# To make a object object_name(var_name="info" ... )
+# e.g Person(person_name="bob",person_text="the builder")
+# To add a item, Person_object.Post_set.create()
+# Works the same with the comment and post
+
 class Person(models.Model):
-    person_name = models.CharField(max_length=20)
-    person_text = models.CharField(max_length=200)
-    post_list = [00000000]
-    def add_id():
-        pass
+    person_name = models.CharField(max_length=20,default="")
+    person_text = models.CharField(max_length=200,default="")
+
     def __str__(self):
         return self.person_name
-class Comment:
-    def __init__(self, person, text):
-        self.person = person
-        self.text = text
-    def return_info():
-        return self.person,self.text
+
+
+class Comment(models.Model):
+
+
+    comment_text = models.CharField(max_length=200,default="")
+
+    def return_info(self):
+        return self.comment_text
+
 
 class Post(models.Model):
-    post_owner = "Person"
-    post_id = 00000000
-#   post_img = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-    post_text = models.CharField(max_length=200)
-    posted_time = models.DateTimeField("previous time")  # when you are getting this date, remeber to subtract to get time since last
-    comment_list = []
-    def comment_add(text):
-        comment_list.append(Comment(person, text))
-    def __str__ (self):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, default="Nobody")
+    post_img = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, default="No picture")
+    post_text = models.CharField(max_length=200, default="no text")
+    # when you are getting this date, remeber to subtract to get time since last
+    posted_time = models.DateTimeField("previous time")
+
+
+    def __str__(self):
         return self.post_text
