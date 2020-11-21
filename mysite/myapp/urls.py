37cf3314122c7e05b4b633e454148c571315270c
Ben Leszczynski <63314709+Benno1472@users.noreply.github.com>
2020-11-20 22:07:23 -0700

    Fixed Registration

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path
-
+from django.urls import path, include 
 from . import views
 
 urlpatterns = [
@@ -7,5 +6,5 @@ urlpatterns = [
     path('all/', views.all_posts, name="all_posts"),
     path('profile/', views.my_profile, name="my_profile"),
     path('create/', views.create_post, name="create_post"),
-    path('registration/login/', views.login, name="login")
+    path('accounts/', include('django.contrib.auth.urls'))   
 ]
